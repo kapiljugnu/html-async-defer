6de9930ad983a94bace31d664705f7d06535e315
Kapil Kumar <kapiljugnu@users.noreply.github.com>
2019-05-21 11:30:17 +0530

    create index2.js

---
+++ index2.js
@@ -0,0 +1,7 @@
+let t2 = +new Date;
+console.log('script2 is running at', t2);
+console.log('script2 element', document.getElementById('load-experiment'));
+while((+new Date) - t2 < 2000) {
+    // delay 2 seconds
+}
+console.log('script2 finishes running at', +new Date);
